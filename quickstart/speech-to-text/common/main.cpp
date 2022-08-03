896167844072b284cf89bfd447c7f239077efa61
Travis Wilson <travisw@microsoft.com>
2022-08-02 17:07:37 -0700

    Initial commit/experiment

---
+++ quickstart/speech-to-text/common/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+#include <speechapi_cxx.h>
+
+using namespace Microsoft::CognitiveServices::Speech;
+using namespace Microsoft::CognitiveServices::Speech::Audio;
+
+auto YourSubscriptionKey = "YourSubscriptionKey";
+auto YourServiceRegion = "YourServiceRegion";
+
+int main()
+{
+    auto speechConfig = SpeechConfig::FromSubscription(YourSubscriptionKey, YourServiceRegion);
+    speechConfig->SetSpeechRecognitionLanguage("en-US");
+
+    auto audioConfig = AudioConfig::FromDefaultMicrophoneInput();
+    auto recognizer = SpeechRecognizer::FromConfig(speechConfig, audioConfig);
+
+    std::cout << "Speak into your microphone.\n";
+    auto result = recognizer->RecognizeOnceAsync().get();
+
+    if (result->Reason == ResultReason::RecognizedSpeech)
+    {
+        std::cout << "RECOGNIZED: Text=" << result->Text << std::endl;
+    }
+    else if (result->Reason == ResultReason::NoMatch)
+    {
+        std::cout << "NOMATCH: Speech could not be recognized." << std::endl;
+    }
+    else if (result->Reason == ResultReason::Canceled)
+    {
+        auto cancellation = CancellationDetails::FromResult(result);
+        std::cout << "CANCELED: Reason=" << (int)cancellation->Reason << std::endl;
+
+        if (cancellation->Reason == CancellationReason::Error)
+        {
+            std::cout << "CANCELED: ErrorCode=" << (int)cancellation->ErrorCode << std::endl;
+            std::cout << "CANCELED: ErrorDetails=" << cancellation->ErrorDetails << std::endl;
+            std::cout << "CANCELED: Did you set the speech resource key and region values?" << std::endl;
+        }
+    }
+}
